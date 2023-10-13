6dd0cfa2c951f702f0de424835f0c5ae0938d3c0
Kristijonas Leipus <kristijonas.leipus@me.com>
2023-10-13 22:30:42 +0300

    Unfinished Select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
+
+const StyledSelect = styled.select`
+  appearance: none;
+  background-color: ${COLORS.transparentGray15};
+  border: none;
+  border-radius: 8px;
+  padding: 12px 16px;
+  width: fit-content;
+
+  &:hover {
+    color: ${COLORS.gray700};
+  }
+`;
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
+    <StyledSelect value={value} onChange={onChange}>
       {children}
-    </select>
+    </StyledSelect>
   );
 };
 
